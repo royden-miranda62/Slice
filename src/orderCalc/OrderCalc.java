7af1235556d82920413e58383ee60464f3a8b446
royden-miranda62 <roydenmiranda62@gmail.com>
2026-02-14 19:03:09 +0400

    Started on Order Calculation feature.
    - Created the file for the individual order calculation feature.

---
+++ src/orderCalc/OrderCalc.java
@@ -0,0 +1,10 @@
+package orderCalc;
+
+public class OrderCalc {
+    public static void main(String[] args) {
+        String[] menu = { "item1", "item2", "item3" };
+
+        System.out.println("Menu: \n");
+
+    }
+}
