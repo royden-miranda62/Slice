1c3f6c225c98301c8aaed034bc979eefefd45f3c
royden-miranda62 <roydenmiranda62@gmail.com>
2026-02-24 10:19:31 +0400

    Added initial declarations.

---
+++ src/totalCalc/CalcTotal.java
@@ -0,0 +1,18 @@
+package totalCalc;
+
+import java.util.Scanner;
+
+class MenuItem {
+    String itemName;
+    int itemQty;
+
+    MenuItem(String n, int q) {
+        itemName = n;
+        itemQty = q;
+    }
+}
+
+public class CalcTotal {
+
+
+}
